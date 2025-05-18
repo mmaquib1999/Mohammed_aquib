4da5f8018845a29988664f92800b51d0c35ed160
Aquib <mmaquib1999@gmail.com>
2025-05-18 14:19:36 +0530

    Changes done

--- Program-1.js
+++ Program-1.js
@@ -1,1 +1,26 @@
-console.log("Hello world")
+class Calculator {
+  constructor(a, b, operation) {
+    this.a = a;
+    this.b = b;
+    this.operation = operation;
+  }
+
+  calculate() {
+    switch (this.operation.toLowerCase()) {
+      case 'add':
+        return this.a + this.b;
+      case 'subtract':
+        return this.a - this.b;
+      case 'multiply':
+        return this.a * this.b;
+      case 'divide':
+        return this.b !== 0 ? this.a / this.b : 'Error: Division by zero';
+      default:
+        return 'Invalid operation';
+    }
+  }
+}
+
+// Example:
+const calc = new Calculator(10.5, 2.5, 'add');
+console.log(calc.calculate());  // Output: 13
