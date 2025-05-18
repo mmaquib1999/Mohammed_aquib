cf8430902678fa75ce6f62c1d52c4b67ca4b693c
Aquib <mmaquib1999@gmail.com>
2025-05-18 15:03:16 +0530

    Changes done

--- Problem-2.js
+++ Problem-2.js
@@ -1,18 +1,16 @@
 function generateOddSeries(a) {
-  
-  const count = a % 2 === 0 ? a - 1 : a;
   const result = [];
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < a; i++) {
     result.push(2 * i + 1);
   }
 
   return result;
 }
 
-//output
+// Output 
 console.log(generateOddSeries(1)); // [1]
-console.log(generateOddSeries(2)); // [1]
+console.log(generateOddSeries(2)); // [1, 3]
 console.log(generateOddSeries(3)); // [1, 3, 5]
-console.log(generateOddSeries(4)); // [1, 3, 5]
+console.log(generateOddSeries(4)); // [1, 3, 5, 7]
 console.log(generateOddSeries(5)); // [1, 3, 5, 7, 9]
